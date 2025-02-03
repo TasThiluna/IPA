011a2f3da3b8b10b5d31d1256ded9cff44566f25
TasThiluna <lufarionite@gmail.com>
2025-02-03 14:13:38 -0500

    I hate Visual Studio:

--- Assets/ipa.cs
+++ Assets/ipa.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using KModkit;
 using rnd = UnityEngine.Random;
 using System.Text.RegularExpressions;
 
